84d5a1c69212bba0f9c958014ab948ad6143bc7e
kimsiyeon <pennya6@hs.ac.kr>
2022-05-24 00:07:47 +0900

    firestore 연결중

--- TestApp/android/app/src/main/java/com/testapp/MainApplication.java
+++ TestApp/android/app/src/main/java/com/testapp/MainApplication.java
@@ -12,7 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.testapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+import androidx.multidex.MultiDexApplication;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
