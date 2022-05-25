62e9b53c2b934799936a2f66875585405bb28a47
kimsiyeon <pennya6@hs.ac.kr>
2022-05-25 23:26:39 +0900

    카운터 만들기 성공

--- TestApp/App.js
+++ TestApp/App.js
@@ -1,14 +1,25 @@
 //컴포넌트는 구성요소
 
-import React from 'react';
-import {SafeAreaView} from 'react-native'; //특별한 컴포넌트들
-import Greeting from './components/Greeting';
+import React,{useState} from 'react';
+import {SafeAreaView,StyleSheet} from 'react-native'; //특별한 컴포넌트들
+import Counter from './components/Counter';
 
 const App=()=>{
+    const [count,setCount]=useState(0);
+
+    const onIncrease=()=>setCount(count+1);
+    const onDecrease=()=>setCount(count-1);
+
 return(
-    <SafeAreaView>
-        <Greeting/>
+    <SafeAreaView style={styles.full}>
+    <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease}/>
     </SafeAreaView>
 );
 };
+const styles=StyleSheet.create({
+    full:{
+        flex:1,
+        backgroundColor:'pink'
+    },
+});
 export default App; //컴포넌트 내보내기
