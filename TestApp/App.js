37b9738e30e59ba6c8a5ebb90100fe5641e76334
kimsiyeon <pennya6@hs.ac.kr>
2022-06-10 17:16:41 +0900

    JAVA alarm app add

--- TestApp/App.js
+++ TestApp/App.js
@@ -2,23 +2,32 @@
 import React from 'react';
 import {Button,SafeAreaView,ToastAndroid,Text} from 'react-native'; //특별한 컴포넌트들
 
-const time='20:33';
-const date=new Date();
+// const time='20:33';
+// const date=new Date();
 
-console.log(date.getHours()+":"+date.getMinutes());
-const App=()=>{
-   if(date.getHours()+":"+date.getMinutes()==time){
-    return (
-        <SafeAreaView>
-            <Text>오운영 학습시간 </Text>
-        </SafeAreaView>
-    );
-   } 
-   return (
-    <SafeAreaView>
-        <Text>오운영 학습시간 아님 </Text>
-    </SafeAreaView>
-);
-};
+// console.log(date.getHours()+":"+date.getMinutes());
+// const App=()=>{
+//    if(date.getHours()+":"+date.getMinutes()==time){
+//     return (
+//         <SafeAreaView>
+//             <Text>오운영 학습시간 </Text>
+//         </SafeAreaView>
+//     );
+//    } 
+//    return (
+//     <SafeAreaView>
+//         <Text>오운영 학습시간 아님 </Text>
+//     </SafeAreaView>
+// );
+// };
+function App(){
+    return(
+        <div className='main'>
+            <div className='header'></div>
+            <div className='body'></div>
+            <div className='footer'></div>
+        </div>
+    )
+}
 
 export default App; //컴포넌트 내보내기
